0b451bb53e915a2ccdcc5291f04df261b8cfdeb9
付国强 <434543162@qq.com>
2026-01-14 21:08:01 +0800

    chore: release with vafast@0.5.1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
  * 为每个请求生成唯一标识符的中间件
  */
 
+import { defineMiddleware } from 'vafast'
+
 /** 请求 ID 生成器函数类型 */
 export type IdGenerator = () => string;
 
@@ -51,9 +53,13 @@ function defaultGenerator(): string {
  * }))
  * 
  * // 在处理函数中使用
- * createHandler({}, async ({ req }) => {
- *   console.log('Request ID:', req.id)
- *   return { requestId: req.id }
+ * defineRoute({
+ *   method: 'GET',
+ *   path: '/',
+ *   handler: ({ req }) => {
+ *     console.log('Request ID:', req.id)
+ *     return { requestId: req.id }
+ *   }
  * })
  * ```
  */
@@ -65,10 +71,7 @@ export function requestId(options: RequestIdOptions = {}) {
     existingHeaderName = headerName,
   } = options;
 
-  return async (
-    req: Request,
-    next: () => Promise<Response>
-  ): Promise<Response> => {
+  return defineMiddleware<{ requestId: string }>(async (req, next) => {
     // 尝试从请求头获取已有的 ID（用于分布式追踪）
     let id: string | undefined;
     
@@ -84,8 +87,8 @@ export function requestId(options: RequestIdOptions = {}) {
     // 将 ID 附加到请求对象
     (req as Request & { id: string }).id = id;
     
-    // 执行下一个中间件/处理函数
-    const response = await next();
+    // 执行下一个中间件/处理函数，传递 requestId 到上下文
+    const response = await next({ requestId: id });
     
     // 将 ID 添加到响应头
     const newHeaders = new Headers(response.headers);
@@ -96,7 +99,7 @@ export function requestId(options: RequestIdOptions = {}) {
       statusText: response.statusText,
       headers: newHeaders,
     });
-  };
+  });
 }
 
 /**
@@ -107,4 +110,3 @@ export function getRequestId(req: Request): string | undefined {
 }
 
 export default requestId;
-
